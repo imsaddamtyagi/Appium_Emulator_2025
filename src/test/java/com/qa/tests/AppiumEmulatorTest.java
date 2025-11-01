e4ffd1904e92cb06b39313a8c24b05dc4bcf9c75
imsaddamtyagi <imsaddamtyagi@gmail.com>
2025-11-01 23:50:49 +0530

    Updated with latest appium --version  (3.1.0)

--- src/test/java/com/qa/tests/AppiumEmulatorTest.java
+++ src/test/java/com/qa/tests/AppiumEmulatorTest.java
@@ -3,13 +3,11 @@ import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
