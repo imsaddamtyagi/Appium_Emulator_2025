7c77ab5998fbe3f1aed4c196c51036c52d416bde
Mohd Husain Ahmad <imsaddamtyagi@gmail.com>
2026-02-17 14:33:02 +0530

    updated dependencies

--- src/test/java/com/qa/tests/AppiumEmulatorTest.java
+++ src/test/java/com/qa/tests/AppiumEmulatorTest.java
@@ -15,11 +15,14 @@ import java.time.Duration;
 
 public class AppiumEmulatorTest
 {
-    AppiumDriver driver;
- 	String apkPath = System.getProperty("user.dir") + "\\TestApk\\app-debug.apk"; 
-	String GitLink =   "new UiSelector().description(\"Visit My GitHub Profile\")";
+    
+ 	String apkPath     =   System.getProperty("user.dir") + "\\TestApk\\Demo.apk"; 
+ 	
+	String GitLink     =   "new UiSelector().resourceId(\"com.bitbar.testdroid:id/editText1\")";
+	String Answer      =   "new UiSelector().resourceId(\"com.bitbar.testdroid:id/button1\")";
+	String AnswerXpath =   "//android.widget.Button[@resource-id=\"com.bitbar.testdroid:id/button1\"]";
 
-
+	AppiumDriver driver;
 
     @BeforeTest
     public void AppiumSetUp() throws URISyntaxException, MalformedURLException 
@@ -27,61 +30,61 @@ public class AppiumEmulatorTest
 
         UiAutomator2Options options = new UiAutomator2Options();
 
-        options.setApp(apkPath); 
-        options.setNoReset(true);       
+        options.setApp(apkPath);      
         options.setPlatformName("Android");
         options.setPlatformVersion("15");
-        options.setDeviceName("Android Emulator");
         options.setAutomationName("UiAutomator2");
+        options.setDeviceName("Android Emulator");
+        options.setNoReset(true);  
 
-        
+         
         URI uri = new URI("http://127.0.0.1:4723");
         System.out.println("Launching Application now...");  
         
         long startTime = System.currentTimeMillis();
         driver = new AndroidDriver(uri.toURL(), options);
         long endTime = System.currentTimeMillis();
-        double executionTimeInSeconds = (endTime - startTime) / 1000.0; // Divide by 1000 for seconds
+        double executionTimeInSeconds = (endTime - startTime) / 1000; // Divide by 1000 for seconds
         
         System.out.println("Time taken in launching application: " + executionTimeInSeconds + " seconds");
     }
 
-
     
     @Test
     public void MyFirstTest_Appium() throws InterruptedException
-    {
-   	 
+    { 	 
     Thread.sleep(10000);
-   	 doClick(driver,GitLink);
-      
-   
+    doSend(driver,GitLink,"Nokia");
+    doClickXpath(driver,AnswerXpath);    
    	 }
     
-    
-    
-    
-    
-    public static   void doClick(AppiumDriver driver, String locator)
-    { 
-   	 
+     
+    public static void doClick(AppiumDriver driver, String locator)
+    {  	 
    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
    WebElement elem = wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.androidUIAutomator(locator))); 
    elem.click();
-
-   
+   System.out.println("Clicked Success");   
     }
     
     
-    public static   void doSend(AppiumDriver driver, String locator,String value)
+    public static void doSend(AppiumDriver driver, String locator,String value)
     { 
-   	 
    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
    WebElement elem = wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.androidUIAutomator(locator))); 
    elem.sendKeys(value);
-
-   
+   System.out.println("Send Success:"+value);     
     }
+    
+    
+    public static void doClickXpath(AppiumDriver driver, String locator)
+    {  	 
+   WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+   WebElement elem = wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.xpath(locator))); 
+   elem.click();
+   System.out.println("Clicked Success using xpath");   
+    }
+    
     
     
 }
